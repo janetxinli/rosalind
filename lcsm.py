5645d7f1e781fdf3cf78f11f3b074bed01ffab92
janetxinli <janetxinli@gmail.com>
2020-04-26 08:26:40 -0700

    Minor edits to lcsm.py

--- lcsm.py
+++ lcsm.py
@@ -44,14 +44,14 @@ def get_matching(contigs, substrings):
     check if substrings are in all contigs and return first substring
     that does. Return False otherwise.
     """
-    for sub in substrings: # Check every possible substring in first contig
-        in_all = True # Present in all contigs so far
-        for c in contigs[1:]: # Every contig after first
+    for sub in substrings:  # Check every possible substring in first contig
+        in_all = True  # Present in all contigs so far
+        for c in contigs[1:]:  # Every contig after first
             if sub not in c:
                 in_all = False
-        if in_all == True: # If substring is present in all contigs, return substring
+        if in_all:  # If substring is present in all contigs, return substring
             return sub
-    return None # If none of the substrings are present in every contig, return None
+    return None  # If none of the substrings are present in every contig, return None
 
 # # contigs = [[1,2,3,4,5], [1,2,4]]
 
@@ -59,14 +59,12 @@ if __name__ == "__main__":
 
     with open(sys.argv[1]) as infile:
         contigs = fasta_to_contigs(infile)
-    # print(contigs)
 
     # Check substrings from first contig
-    checker = contigs[0] # First contig; acts as checker
-    for i in range(len(checker), 0, -1):
+    checker = contigs[0]  # First contig; acts as checker
+    for i in range(len(checker), 0, -1):  # Decrease length of substring by 1 each time
         subs = get_substrings(checker, i)
-        # print(subs)
-        common_substring = get_matching(contigs, subs)
-        if common_substring != None:
-            print(common_substring, file=sys.stdout)
+        found_common = get_matching(contigs, subs)
+        if found_common != None:
+            print(found_common)
             sys.exit(0)
