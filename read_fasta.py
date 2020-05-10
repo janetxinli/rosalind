f0cccd95d7cc94ae0c2766370430e8066e7cefd7
janetxinli <janetxinli@gmail.com>
2020-05-10 08:55:04 -0700

    Allow several sequences in one fasta file.

--- read_fasta.py
+++ read_fasta.py
@@ -9,13 +9,34 @@ def read_fasta(file_obj):
     Takes in a list containing the contents of a file and returns the header and sequence
     of that file in a tuple.
     """
-    header = ""
-    seq = ""
+    header = []
+    seq = []
 
-    for line in file_object:
-        if line[0] == ">":
-            header += line.strip()
+    current = file_obj.readline()
+    current_seq = None
+
+    # Iterate over each line in file object
+    while current:
+        # current_seq = None
+        # Header line
+        if current[0] == ">":
+            if current_seq:
+                seq.append(current_seq)
+            current_seq = None
+            header.append(current[1:].strip())
+
+        # First sequence line
+        elif not current_seq:
+            current_seq = current.strip()
+
         else:
-            seq += line.strip()
+            current_seq += current.strip()
 
-    return header, seq
+        current = file_obj.readline()
+
+    # Append last sequence to seq list
+    seq.append(current_seq)
+
+    while len(header) > 0 and len(seq) > 0:
+        yield header.pop(0), seq.pop(0)
+
