066964bc3b3666c6b918cbf669cd0a0d2a7c9261
janetxinli <janetxinli@gmail.com>
2020-09-09 12:29:41 -0700

    Clean up code

--- gc.py
+++ gc.py
@@ -1,43 +1,30 @@
 #!/usr/bin/env python3
 """
-Given a fasta file, compute GC content and return the sequence with highest GC-content and its ID.
-usage: cat [fasta file] | python3 gc.py
-Author: janetxinli
+Computing GC content
+Usage: ./gc.py [input file]
 """
 
 import sys
+from tools import read_fasta
 
 def get_gc(seq):
-    gc_count = seq.count("C")
-    gc_count += seq.count("G")
-    return((gc_count/len(seq)) * 100)
+    """Get the GC content for a given sequence."""
+    gc_count = 0
+    for base in seq:
+        if base == "G" or base == "C":
+            gc_count += 1
+    return (gc_count/len(seq)) * 100
 
-def run_gc():
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: %s [input file]" % sys.argv[0])
+        sys.exit(1)
     gc_content = {}
-    last_seq = ""
-
-    # Read first line and load ID
-    last_ID = sys.stdin.readline().strip()[1:]
-
-    # Read rest of fasta file
-    for line in sys.stdin:
-        if line[0] != ">":
-            # Load sequence contained in line
-            last_seq += line.strip().upper()
-        else:
-            # Header line; deposit ID and gc content of sequence into gc_content
-            last_seq_gc = get_gc(last_seq)
-            gc_content[last_seq_gc] = last_ID
-            last_seq = ""
-            last_ID = line.strip()[1:]
-
-    # Deposit ID and gc content of last sequence into gc_content
-    last_seq_gc = get_gc(last_seq)
-    gc_content[last_seq_gc] = last_ID
-
-    max_GC = max(gc_content)
-    print(gc_content[max_GC], max_GC, sep="\n")
-    return
+    for header, seq in read_fasta(sys.argv[1]):
+        gc_content[get_gc(seq)] = header
+    max_gc = max(gc_content)
+    print(gc_content[max_gc])
+    print(max_gc)
 
 if __name__ == "__main__":
-    run_gc()
+    main()
