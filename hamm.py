b7b7b214c56c06b389ba9de3fa2eeabf93d1b11d
janetxinli <janetxinli@gmail.com>
2020-09-09 13:23:11 -0700

    Clean up code

--- hamm.py
+++ hamm.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
 """
-Given two strings of equal length, determine the Hamming distance (number of corresponding characters that differ
-between the two). Return the Hamming distance as an integer.
+Counting point mutations
+Usage: ./hamm.py [input file]
 """
 
+import sys
+
 def hamming_distance(s, t):
+    """Calculate hamming distance between strings of equal length s and t."""
     if len(s) != len(t):
-        raise RuntimeError("Two input strings must be of same length.")
-
+        raise TypeError("Two input strings must be of same length.")
     hamm = 0
-
     for i, char in enumerate(s):
-        if t[i].upper() == char.upper():
-            continue
-        else:
-            hamm += 1
-
+        if t[i].upper() != char.upper():
+            hamm += 1    
     return hamm
+
+def main():
+    """Calculate hamming distance for input file."""
+    if len(sys.argv) != 2:
+        print("Usage: %s [input file]" % sys.argv[0])
+        sys.exit(1)
+    with open(sys.argv[1]) as infile:
+        string_1 = infile.readline().strip()
+        string_2 = infile.readline().strip()
+        print(hamming_distance(string_1, string_2))
+
+if __name__ == "__main__":
+    main()
