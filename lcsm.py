9fdec1f353e9d1dac7fb22604d52814a20c64976
janetxinli <janetxinli@gmail.com>
2020-09-13 15:34:42 -0700

    Use read_fasta script and clean up code

--- lcsm.py
+++ lcsm.py
@@ -5,24 +5,7 @@ Usage: ./lcsm.py [input_file]
 """
 
 import sys
-
-def fasta_to_contigs(infile):
-    """
-    Takes input file object in fasta format and returns a list
-    of all contigs in the file.
-    """
-    contig = ""
-    contig_list = []
-    for line in infile:
-        if line[0] == ">":  # Header line
-            contig_list.append(contig)
-            contig = ""
-        else:
-            contig += line.strip()
-    contig_list.append(contig)
-    del contig_list[0]
-    return contig_list
-
+from tools import read_fasta
 
 def get_substrings(contig, length):
     """
@@ -35,35 +18,39 @@ def get_substrings(contig, length):
         subs.append(contig[i:i+length])
     return subs
 
-# print(get_substrings("CCATA", 3))
-
 def get_matching(contigs, substrings):
     """
     Given a list of contigs and a list of substrings,
     check if substrings are in all contigs and return first substring
     that does. Return False otherwise.
     """
-    for sub in substrings:  # Check every possible substring in first contig
-        in_all = True  # Present in all contigs so far
-        for c in contigs[1:]:  # Every contig after first
+    for sub in substrings:
+        in_all = True
+        for c in contigs[1:]:
             if sub not in c:
                 in_all = False
-        if in_all:  # If substring is present in all contigs, return substring
+        if in_all:
             return sub
-    return None  # If none of the substrings are present in every contig, return None
+    return None
 
-# # contigs = [[1,2,3,4,5], [1,2,4]]
-
-if __name__ == "__main__":
-
-    with open(sys.argv[1]) as infile:
-        contigs = fasta_to_contigs(infile)
-
-    # Check substrings from first contig
-    checker = contigs[0]  # First contig; acts as checker
-    for i in range(len(checker), 0, -1):  # Decrease length of substring by 1 each time
+def get_common(contigs, checker):
+    for i in range(len(checker), 0, -1):
         subs = get_substrings(checker, i)
         found_common = get_matching(contigs, subs)
         if found_common != None:
-            print(found_common)
-            sys.exit(0)
+            return(found_common)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: %s [input file]" % sys.argv[0])
+        sys.exit(1)
+    contigs = []
+    for _, seq in read_fasta(sys.argv[1]):
+        contigs.append(seq)
+    
+    # Check shortest sequence against other sequences
+    checker = min(contigs, key=lambda x: len(x))
+    print(get_common(contigs, checker))
+
+if __name__ == "__main__":
+    main()
